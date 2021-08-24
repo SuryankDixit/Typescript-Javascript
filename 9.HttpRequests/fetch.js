4091538d3eaab3443c77ed72bed8ad5920f0c7f3
SuryankDixit <suryankdixit@gmail.com>
2021-08-24 18:23:08 +0530

    chaining of fetch statements

--- 9.HttpRequests/fetch.js
+++ 9.HttpRequests/fetch.js
@@ -2,22 +2,50 @@
 AJAX : asynchronous js and xml;
 */
 
+// fetch('https://swapi.dev/api/planets/')
+//     .then(function (response) {
+//         if (!response.ok) {
+//             throw new Error(`Status Code Error: ${response.status}`);
+//         } else {
+//             response.json()
+//                 .then(function (data) {
+//                     console.log(data);
+//                     data.results.forEach(function (planet) {
+//                         console.log(planet.name, planet.climate);
+//                     })
+//                 })
+//                 .catch(function (err) {
+//                     console.log(err);
+//                 });
+//                 console.log("Hello2");
+//         }
+//     })
+//     .catch(function (err) {
+//         console.log(err);
+//     });
+
+// console.log("Hello");
+
+
 fetch('https://swapi.dev/api/planets/')
     .then(function (response) {
-        if (!response.ok) {
-            throw new Error(`Status Code Error: ${response.status}`);
-        } else {
-            response.json()
-                .then(function (data) {
-                    console.log(data);
-                    data.results.forEach(function (planet) {
-                        console.log(planet.name, planet.climate);
-                    })
-                })
-                .catch(function (err) {
-                    console.log(err);
-                });
-        }
+        if (!response.ok) throw new Error(`Status Code Error: ${response.status}`);
+        else return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        const filmURL = data.results[0].films[0];
+        return fetch(filmURL);
+    })
+    .then(function (response) {
+        if (!response.ok) throw new Error(`Status Code Error: ${response.status}`);
+        else return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        console.log(data.title);
+        // const filmURL = data.results[0].films[0];
+        // return fetch(filmURL);
     })
     .catch(function (err) {
         console.log(err);
